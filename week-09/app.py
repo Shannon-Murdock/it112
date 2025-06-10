1b12f419b5ec00e57d394fbe1484e03f4a957ab0
Shannon-Murdock <shannon.murdock@seattlecolleges.edu>
2025-06-10 04:43:43 -0700

    Putting correct guessingGame.py file

---
+++ week-09/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Configure SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize database
+db = SQLAlchemy(app)
+
+# Define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+# Create database tables
+with app.app_context():
+    db.create_all()
+
+# Routes
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    if name:
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
